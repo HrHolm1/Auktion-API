92680b1f9a40fa351e7e55c629975a1a6b94db48
HrHolm1 <robinholm123456789@gmail.com>
2025-11-19 10:48:59 +0100

    Update AuctionController.cs
    Upated controller

--- Auktion-API/Controllers/AuctionController.cs
+++ Auktion-API/Controllers/AuctionController.cs
@@ -1,6 +1,65 @@
-﻿namespace Auktion_API.Controllers;
+﻿using Auktion_API.Models;
+using Auktion_API.Services;
+using Microsoft.AspNetCore.Mvc;
 
-public class AuctionController
+namespace Auktion_API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuctionController : ControllerBase
 {
-    
+    private readonly AuctionService _auctionService;
+
+    public AuctionController(AuctionService auctionService)
+    {
+        _auctionService = auctionService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Getall()
+    {
+        var auctions = _auctionService.GetAllAsync();
+        return Ok(auctions);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var auction = _auctionService.GetByIdAsync(id);
+        if (auction == null)
+            return NotFound();
+        return Ok(auction);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Auction auction)
+    {
+        var newAuction = await _auctionService.CreateAsync(auction);
+        return CreatedAtAction(nameof(GetById), new { id = newAuction.Id }, newAuction);
+    }
+
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(int id, Auction auction)
+    {
+        if (id != auction.Id)
+            return BadRequest();
+
+        var succes = await _auctionService.UpdateAsync(auction);
+        if (!succes)
+            return NotFound();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var deleted = await _auctionService.DeleteAsync(id);
+        if (!deleted)
+            return NotFound();
+
+        return NoContent();
+    }
+
 }
