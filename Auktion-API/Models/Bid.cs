1e5df6d77bd2760988d5b8de30ab12aa02e637c0
HrHolm1 <robinholm123456789@gmail.com>
2025-11-20 09:54:21 +0100

    added bid model
    Created bid model

---
+++ Auktion-API/Models/Bid.cs
@@ -0,0 +1,18 @@
+﻿namespace Auktion_API.Models;
+
+public class Bid
+{
+    public int Id { get; set; }
+
+    public int LotNr { get; set; } //Ikke sikker på om det skal være LotNr eller LotId
+    
+    public string BidderName { get; set; } //User id måske hvis vi oprettet en user model
+
+    public double Amount { get; set; }
+
+    public DateTime PlacedAt { get; set; } //Datetime til at hvornår buddet er sat op, bliver til createdAt i DB.
+    
+    public List<Lot> Lots { get; set; } = new(); // Navigation Property
+
+    
+}
