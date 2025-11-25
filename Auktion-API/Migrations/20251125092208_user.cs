96764a8a1580189206b96544ddc11af7945d7f73
madsvodder <madsvodder@gmail.com>
2025-11-25 10:23:00 +0100

    User model migration

---
+++ Auktion-API/Migrations/20251125092208_user.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Auktion_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class user : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                schema: "auctions",
+                table: "Bids",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                schema: "auctions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bids_UserId",
+                schema: "auctions",
+                table: "Bids",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bids_Users_UserId",
+                schema: "auctions",
+                table: "Bids",
+                column: "UserId",
+                principalSchema: "auctions",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bids_Users_UserId",
+                schema: "auctions",
+                table: "Bids");
+
+            migrationBuilder.DropTable(
+                name: "Users",
+                schema: "auctions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bids_UserId",
+                schema: "auctions",
+                table: "Bids");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                schema: "auctions",
+                table: "Bids",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
