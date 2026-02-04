f61bf00d121a822bfae98c6f5ffeab52a7289a3e
madsvodder <madsvodder@gmail.com>
2026-02-04 12:08:54 +0100

    Backend

--- Auktion-API/DTOs/RefreshRequestDto.cs
+++
@@ -1,6 +0,0 @@
-namespace Auktion_API.DTOs;
-
-public class RefreshRequestDto
-{
-    public string token { get; set; } = "";
-}
--- Auktion-API/Models/LoginResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace Auktion_API.Models;
-
-public class LoginResponse
-{
-    public string Token { get; set; }
-    public string Role {get; set;}
-    public int userId { get; set; }
-    public string username { get; set; }
-}
